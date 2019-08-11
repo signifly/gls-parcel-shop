4518bf0a1a5eb74634b6fd07668f85471709331a
Morten Poul Jensen <mpj@signifly.com>
2019-08-11 12:38:19 +0200

    wip

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -2,16 +2,34 @@
 
 namespace Signifly\ParcelShop\Resources;
 
+/**
+ * @internal
+ */
 abstract class Resource
 {
-    /** @var array */
+    /**
+     * The data for the resource.
+     *
+     * @var array
+     */
     protected $data;
 
+    /**
+     * Create a new Resource instance.
+     *
+     * @param array $data
+     */
     public function __construct(array $data)
     {
         $this->data = $data;
     }
 
+    /**
+     * Get data for the resource by the provided key.
+     *
+     * @param  string $key
+     * @return mixed
+     */
     public function getData(string $key)
     {
         return data_get($this->data, $key);
